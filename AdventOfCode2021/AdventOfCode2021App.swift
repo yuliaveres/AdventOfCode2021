01379784327a77b48eec2617cea1979cadaca3b4
Yuliia Veresklia <yuliia.veresklia@kolonial.no>
2021-12-01 17:07:05 +0100

    Initial Commit

---
+++ AdventOfCode2021/AdventOfCode2021App.swift
@@ -0,0 +1,17 @@
+//
+//  AdventOfCode2021App.swift
+//  AdventOfCode2021
+//
+//  Created by Yuliia Veresklia on 01/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct AdventOfCode2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
